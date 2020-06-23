98b8402a472b77e8f229e95a635ff38f141c8d1a
Jon Kesler <jkesler@hotmail.com>
2020-06-23 09:16:51 -0400

    View queries working

--- server.js
+++ server.js
@@ -56,29 +56,69 @@ function start() {
       case "Add Employee":
         addEmployee();
         break;
-
-      case "Remove Employee":
-        removeEmployee();
+      
+      case "Add Department":
+        addDepartment();
         break;
+
+      case "Add Role":
+        addRole();
+        break;
+
+      // case "Remove Employee":
+      //   removeEmployee();
+      //   break;
 
       case "Update Employee Role":
         updateEmployee();
         break;
 
-      case "Update Employee Manager":
-        updateManager();
-        break;
+      // case "Update Employee Manager":
+      //   updateManager();
+      //   break;
+
+      
+      // case "View Budget of department":
+      //   viewBudget();
+      //   break;
       }
     });
 }
 
 function allEmployees() {
-    var query = "SELECT * FROM employee";
+    var query = "SELECT first_name, last_name, department.name, role.salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id";
     connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].artist);
-      }
-      runSearch();
+      console.log(res)
+      console.table(res, ["first_name:", "last_name:", "role.title:", "department.name"]);
+      start();
     });
   } 
+
+function allDepartment (){
+  // not working.  try re writing the query and put department table first.
+  var query = "SELECT first_name, last_name, department.name, role.salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY role.department";
+  connection.query(query, function(err, res) {
+    console.log(res)
+    console.table(res, ["first_name:", "last_name:", "role.title:", "department.name"]);
+    start();
+  });
+}
+
+function allManager (){
+  var query = "SELECT first_name, last_name, department.name, role.salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY manager_id";
+  connection.query(query, function(err, res) {
+    console.log(res)
+    console.table(res, ["first_name:", "last_name:", "role.title:", "department.name"]);
+    start();
+  });
+}
+
+function addEmployee (){
+  var query = "SELECT first_name, last_name, department.name, role.salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY manager_id";
+  connection.query(query, function(err, res) {
+    console.log("Employee added!")
+    console.table(res, ["first_name:", "last_name:", "role.title:", "department.name"]);
+    start();
+  });
+}
 
